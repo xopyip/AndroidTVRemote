8352ab03e4adea222d44e3645f95b4de228dc0c9
Mateusz Bałuch <xopyip@users.noreply.github.com>
2022-04-09 23:40:03 +0200

    implement recursive static file mapping

--- app/src/main/java/me/xopyip/webremote/MainActivity.kt
+++ app/src/main/java/me/xopyip/webremote/MainActivity.kt
@@ -19,42 +19,10 @@ class MainActivity : FragmentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /* ===================================================================== */
-
         val path = applicationContext.filesDir.absolutePath + "/"
         val executorService: ExecutorService = Executors.newSingleThreadExecutor()
         executorService.execute {
             val app = Javalin.create().start(7070)
-            val codes = mapOf(
-                "volup" to KEYCODE_VOLUME_UP,
-                "voldown" to KEYCODE_VOLUME_DOWN,
-                "mute" to KEYCODE_VOLUME_MUTE,
-
-                "chup" to KEYCODE_CHANNEL_UP,
-                "chdown" to KEYCODE_CHANNEL_DOWN,
-                "menu" to KEYCODE_MENU,
-                "apps" to KEYCODE_APP_SWITCH,
-
-                "up" to KEYCODE_DPAD_UP,
-                "down" to KEYCODE_DPAD_DOWN,
-                "left" to KEYCODE_DPAD_LEFT,
-                "right" to KEYCODE_DPAD_RIGHT,
-                "center" to KEYCODE_DPAD_CENTER,
-                "enter" to KEYCODE_ENTER,
-                "playpause" to KEYCODE_MEDIA_PLAY_PAUSE,
-                "stop" to KEYCODE_MEDIA_STOP,
-                "next" to KEYCODE_MEDIA_NEXT,
-                "prev" to KEYCODE_MEDIA_PREVIOUS,
-                "rewind" to KEYCODE_MEDIA_REWIND,
-                "ff" to KEYCODE_MEDIA_FAST_FORWARD,
-
-                "onoff" to KEYCODE_TV_POWER,
-
-
-                "back" to KEYCODE_BACK,
-                "home" to KEYCODE_HOME
-            )
-
             val sock = Socket("localhost", 5555)
             sock.writePacket(MessageType.CONNECT, 0x01000000, 4096, "host::".toByteArray())
             val inputStream = sock.getInputStream()
@@ -66,14 +34,24 @@ class MainActivity : FragmentActivity() {
             while (!connected) {
                 if (inputStream.available() > 0) {
                     val packetFromBuffer = AdbPacket.fromBuffer(inputStream)
-                    when(MessageType.values().first { it.cmd == packetFromBuffer.command }){
+                    when (MessageType.values().first { it.cmd == packetFromBuffer.command }) {
                         MessageType.AUTH -> {
                             Log.i("WebRemote", "Auth")
                             if (packetFromBuffer.arg1 == 1) {
-                                if(authChecked){
-                                    sock.writePacket(MessageType.AUTH, 3, 0, Crypto.getAdbPublic(keys.public as RSAPublicKey))
-                                }else{
-                                    sock.writePacket(MessageType.AUTH, 2, 0, Crypto.encrypt(packetFromBuffer.data, keys.private))
+                                if (authChecked) {
+                                    sock.writePacket(
+                                        MessageType.AUTH,
+                                        3,
+                                        0,
+                                        Crypto.getAdbPublic(keys.public as RSAPublicKey)
+                                    )
+                                } else {
+                                    sock.writePacket(
+                                        MessageType.AUTH,
+                                        2,
+                                        0,
+                                        Crypto.encrypt(packetFromBuffer.data, keys.private)
+                                    )
                                     authChecked = true
                                 }
                             }
@@ -92,19 +70,10 @@ class MainActivity : FragmentActivity() {
                     }
                 }
             }
-            var local_id = 0
 
-            app.get("/") { ctx ->
-                val code = ctx.queryParam("code")
-                if (code != null && codes.containsKey(code)) {
-                    if(connected){
-                        sock.exec("input keyevent " + codes[code], local_id++);
-                    }
-                }
-                ctx.html(codes.map { (k) -> "<a href=\"/?code=${k}\">${k}</a>" }.joinToString())
-            }
+            app.get("/") { it.redirect("/index.html") }
+            registerStaticFiles(app, "")
         }
-        /* ===================================================================== */
 
 
 
@@ -112,6 +81,23 @@ class MainActivity : FragmentActivity() {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.main_browse_fragment, MainFragment())
                 .commitNow()
+        }
+    }
+
+    private fun registerStaticFiles(app: Javalin, path: String) {
+        val files = assets.list("public$path")
+        files?.forEach { it ->
+            if (assets.list("public$path/$it")!!.isEmpty()) {
+                app.get("$path/$it") { ctx ->
+                    when {
+                        it.endsWith(".html") -> ctx.html(
+                            assets.open("public$path/$it").bufferedReader().use { it.readText() })
+                        else -> ctx.result(assets.open("public$path/$it"))
+                    }
+                }
+            } else {
+                registerStaticFiles(app, "$path/$it")
+            }
         }
     }
 }
