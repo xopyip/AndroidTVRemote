0321ca95b05f7b874144744a61ebf518264fc4fe
Mateusz Bałuch <xopyip@users.noreply.github.com>
2022-04-10 13:43:58 +0200

    proper websocket connection handling

--- app/src/main/assets/public/app.js
+++ app/src/main/assets/public/app.js
@@ -1,5 +1,23 @@
-var exampleSocket = new WebSocket("ws://" + window.location.host + "/ws");
-//todo: proper connection handling
+var ws = null;
+function connect(){
+    ws = new WebSocket("ws://" + window.location.host + "/ws");
+
+    ws.onclose = function(e) {
+        setTimeout(function() {
+          connect();
+        }, 1000);
+    };
+
+    ws.onerror = function(err) {
+        ws.close();
+    };
+}
+connect();
+
 function handleButton(str){
-    exampleSocket.send("click," + str);
+    if(ws == null){
+        alert("WebSocket isn't connected");
+        return;
+    }
+    ws.send("click," + str);
 }
